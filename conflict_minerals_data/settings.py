ecd6160a3a81b7c56e44d090a8a0efe8ea03b74e
Sarah Bird <sarah@bonvaya.com>
2017-08-05 18:28:34 -0500

    Reconfigure whitenoise

--- conflict_minerals_data/settings.py
+++ conflict_minerals_data/settings.py
@@ -36,6 +36,7 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
+    'whitenoise.runserver_nostatic',
     'django.contrib.staticfiles',
     'channels',
     'rest_framework',
@@ -47,6 +48,7 @@ if DEBUG is True:
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
