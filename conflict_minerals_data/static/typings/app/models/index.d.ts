81c1bf11ac678847105514d1f59b88b51537fd58
Sarah Bird <sarah@bonvaya.com>
2017-08-12 14:42:24 -0500

    Add missed compiled index

--- conflict_minerals_data/static/typings/app/models/index.d.ts
+++ conflict_minerals_data/static/typings/app/models/index.d.ts
@@ -1,17 +1,43 @@
-export interface Company {
-    cik: string;
-    conformed_name: string;
-    ticker_symbol?: string;
-    sic_code?: string;
-    sic_description?: string;
-    state_locatin?: string;
-}
 export interface DjangoAPIResponse {
     count: number;
     next: string | null;
     previous: string | null;
     results: any[];
 }
+export interface Company {
+    id: number;
+    cik: string;
+    conformed_name: string;
+    ticker_symbol?: string;
+    sic_code?: string;
+    sic_description?: string;
+    state_location?: string;
+}
 export interface CompanyResponse extends DjangoAPIResponse {
     results: Company[];
+}
+export interface Filing {
+    id: number;
+    filing_type: string;
+    sec_accession_number: string;
+    company: number;
+    date: Date;
+    link: string;
+}
+export interface FilingResponse extends DjangoAPIResponse {
+    results: Filing[];
+}
+export interface Document {
+    id: number;
+    filing: number;
+    seq: number;
+    description: string;
+    doc_type: string;
+    doc_size: number;
+    doc_name: string;
+    doc_url: string;
+    doc_format: string;
+}
+export interface DocumentResponse extends DjangoAPIResponse {
+    results: Document[];
 }
